64af73d5a14fffe36ecb6efabe1e81b4a807a35d
anamaria-28 <anamaria282001@gmail.com>
2025-04-01 21:26:43 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
+import { UserProfile } from "./types";
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
