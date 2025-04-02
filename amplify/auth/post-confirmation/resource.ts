8ba6cc8d4d6f56c54ca1bb463985f877708b1c03
anamaria-28 <anamaria282001@gmail.com>
2025-04-02 21:20:12 +0300

    Update resource.ts

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -2,4 +2,8 @@ import { defineFunction } from '@aws-amplify/backend';
 
 export const postConfirmation = defineFunction({
   name: 'post-confirmation',
+  handler: async (event) => {
+    console.log("Post confirmation trigger fired:", event);
+    return event;
+  },
 });
