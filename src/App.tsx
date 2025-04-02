36114632968a4ed52ee0eaee6e94d061253e53f7
anamaria-28 <anamaria282001@gmail.com>
2025-04-02 20:09:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,24 @@
-import { useState, useEffect } from "react";
-import {
-  Button,
-  Heading,
-  Flex,
-  View,
-  Grid,
-  Divider,
-} from "@aws-amplify/ui-react";
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import { Amplify } from "aws-amplify";
-import "@aws-amplify/ui-react/styles.css";
-import { generateClient } from "aws-amplify/data";
-import outputs from "../amplify_outputs.json";
-import { UserProfile } from "./types";
-/**
- * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
- */
+import type { Schema } from '../amplify/data/resource';
 
-Amplify.configure(outputs);
-const client = generateClient({
-  authMode: "userPool",
-});
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useEffect, useState } from 'react';
+import { generateClient } from 'aws-amplify/data';
 
-export default function App() {
-  const [userprofiles, setUserProfiles] = useState([]);
-  const { signOut } = useAuthenticator((context) => [context.user]);
+const client = generateClient<Schema>();
 
-  useEffect(() => {
-    fetchUserProfile();
-  }, []);
+function App() {
 
-  async function fetchUserProfile() {
-    const { data: profiles } = await client.models.UserProfile.list();
-    setUserProfiles(profiles);
-  }
+  const { signOut } = useAuthenticator();
+
+  // ...
 
   return (
-    <Flex
-      className="App"
-      justifyContent="center"
-      alignItems="center"
-      direction="column"
-      width="70%"
-      margin="0 auto"
-    >
-      <Heading level={1}>My Profile</Heading>
+    <main>
+      {/* ... */}
 
-      <Divider />
-
-      <Grid
-        margin="3rem 0"
-        autoFlow="column"
-        justifyContent="center"
-        gap="2rem"
-        alignContent="center"
-      >
-        {userprofiles.map((userprofile) => (
-          <Flex
-            key={userprofile.id || userprofile.email}
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            gap="2rem"
-            border="1px solid #ccc"
-            padding="2rem"
-            borderRadius="5%"
-            className="box"
-          >
-            <View>
-              <Heading level="3">{userprofile.email}</Heading>
-            </View>
-          </Flex>
-        ))}
-      </Grid>
-      <Button onClick={signOut}>Sign Out</Button>
-    </Flex>
+      <button onClick={signOut}>Sign out</button>
+    </main>
   );
 }
+
+export default App;
