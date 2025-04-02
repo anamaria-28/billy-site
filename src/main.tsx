e47cef3784f1d0e25adccf75e1279544155c938b
anamaria-28 <anamaria282001@gmail.com>
2025-04-02 20:04:31 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 import App from "./App.tsx";
+import outputs from '../amplify_outputs.json';
 import "./index.css";
-import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
