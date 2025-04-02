a843e3ff6f0ac99093c0cb0d431ad3f1477b0fa3
anamaria-28 <anamaria282001@gmail.com>
2025-04-02 20:12:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { generateClient } from 'aws-amplify/data';
 const client = generateClient<Schema>();
 
 function App() {
+    const { user, signOut } = useAuthenticator();
 
   const { signOut } = useAuthenticator();
 
@@ -14,8 +15,8 @@ function App() {
 
   return (
     <main>
+       <h1>{user?.signInDetails?.loginId}'s todos</h1>
       {/* ... */}
-
       <button onClick={signOut}>Sign out</button>
     </main>
   );
