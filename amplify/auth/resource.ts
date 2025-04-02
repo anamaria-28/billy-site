0a60063fc5d5cb8b26a970c213dbb17c9af644f0
anamaria-28 <anamaria282001@gmail.com>
2025-04-02 21:32:09 +0300

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,5 +1,9 @@
 import { defineAuth } from '@aws-amplify/backend';
 
+const postConfirmation = async (event) => {
+  console.log("Post confirmation trigger fired:", event);
+  return event;
+}
 export const auth = defineAuth({
   loginWith: {
     email: true,
